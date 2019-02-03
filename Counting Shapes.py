f0e46c42bf92e4204d975e12b374ec6528bd1c16
Marina Gamal <32170558+MarinaGamal@users.noreply.github.com>
2019-02-03 12:01:57 +0200

    Updating the code for better detection from Stream
    Changed the detection to take place from a gray image instead of searching for the black color's range.

--- Counting Shapes.py
+++ Counting Shapes.py
@@ -37,9 +37,18 @@ def detect(c):
 # In[3]:
 
 
-def detectShapes(image, mask):
-    res = cv2.bitwise_and(image, image, mask=mask)
-    cv2.imshow("Result", res)
+def DoG(img):
+    blur5 = cv2.GaussianBlur(img,(5,5),0)
+    blur3 = cv2.GaussianBlur(img,(3,3),0)
+    DoGim = blur5 - blur3
+    return DoGim
+
+
+# In[4]:
+
+
+def detectShapes(mask):
+
 
     X = 20
     Y = 20
@@ -47,34 +56,53 @@ def detectShapes(image, mask):
     rectangle = 0
     circle = 0
     triangle = 0
-    kernel = np.ones((11, 11), np.uint8)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow("Closing", closing)
-    edges = cv2.Canny(closing, 100, 200)
-    blur = cv2.blur(edges, (5, 5), 1)
+    kernelopen = np.ones((5, 5), np.uint8)
+    kernelClose = np.ones((3, 3),np.uint8)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelopen)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernelClose)
+    R=closing.shape[0]
+    C=closing.shape[1]
+    temp = closing[30:R-30,30:C-30]
+    edges =DoG(temp)
     cv2.imshow("edg", edges)
-#     lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=300, maxLineGap=0)
-    lines = cv2.HoughLinesP(edges,1,np.pi/180,50,minLineLength=5,maxLineGap=50)
+    ratio = image.shape[0] / float(image.shape[0])
+    
+    
+#     lines = cv2.HoughLinesP(edges,1,np.pi/180,50,minLineLength=20,maxLineGap=60)
 
-    if (lines is None):
-        return
-    N = lines.shape[0]
-    for i in range(N):
-        x1 = lines[i][0][0]
-        y1 = lines[i][0][1]
-        x2 = lines[i][0][2]
-        y2 = lines[i][0][3]
-        cv2.line(edges, (x1, y1), (x2, y2), (255, 0, 0), 2)
+#     if (lines is None):
+#         return
+#     N = lines.shape[0]
+#     for i in range(N):
+#         x1 = lines[i][0][0]
+#         y1 = lines[i][0][1]
+#         x2 = lines[i][0][2]
+#         y2 = lines[i][0][3]
+#         cv2.line(edges, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
     mx = np.amax(edges)
     mn = np.amin(edges)
 
     thresh_val = np.average(np.arange(mn, mx))
     _, thresh = cv2.threshold(edges, thresh_val, 255, cv2.THRESH_BINARY)
-
+    
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    
+    
+#     for c in cnts:
+#         M=cv2.moments(c)
+# #         cX = int((M["m10"]/M["m00"])*ratio)
+# #         cY = int((M["m01"]/M["m00"])*ratio)
+        
+#         shape = detect(c)
+#         c = c.astype("float")
+#         c *= ratio
+#         c = c.astype("int")
+
+#         cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+# #         cv2.putText(image, color +" " + shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 0), 2)
+
 
     for c in cnts:
         if cv2.arcLength(c, True)>50:       #this is the condition to eliminate small curves 
@@ -96,7 +124,6 @@ def detectShapes(image, mask):
 
     cv2.putText(image, str(rectangle), (X, Y + 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.rectangle(image, (X + 25, Y + 60), (X + 55, Y + 90), (0, 0, 255), -1)
-    # cv2.putText(image, str(triangle) + " " + "triangle", (X, Y + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     pt1 = (X + 40, Y + 100)
     pt2 = (X + 20, Y + 130)
@@ -106,53 +133,43 @@ def detectShapes(image, mask):
     cv2.putText(image, str(triangle) + " ", (X, Y + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 
-# In[4]:
+# In[ ]:
 
-
-
-# In[4]:
 
 cap = cv2.VideoCapture(0)
 while True:
-    _, image = cap.read()
-    # image = cv2.imread('shapes2.jpg')
+    for i in range(15):
+         cap.grab()
+    (grabbed, image) = cap.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    ratio = image.shape[0] / float(image.shape[0])
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mx = np.amax(gray)
+    mn = np.amin(gray)
+
+    thresh_val = np.average(np.arange(mn, mx))
+    _, thresh = cv2.threshold(gray, thresh_val, 255, cv2.THRESH_BINARY)
     color = ""
-
-    lower_black = np.array([0, 0, 0])
-    upper_black = np.array([10, 10, 60])
-    # lower_black = np.array([0, 0, 0])
-    # upper_black = np.array([80, 50, 100])
-
-    mask1 = cv2.inRange(hsv, lower_black, upper_black)
-    cv2.imshow("mask1", mask1)
-    detectShapes(image, mask1)
-
+    detectShapes(thresh)
     cv2.imshow('image', image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
 
 # image = cv2.imread('10.jpg')
 
-# ratio = image.shape[0] / float(image.shape[0])
-# hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# mx = np.amax(gray)
+# mn = np.amin(gray)
+
+# thresh_val = np.average(np.arange(mn, mx))
+# _, thresh = cv2.threshold(gray, thresh_val, 255, cv2.THRESH_BINARY)
 # color = ""
 
-# lower_black = np.array([0, 0, 0])
-# upper_black = np.array([180, 255, 30])
-
-# mask1 = cv2.inRange(hsv, lower_black, upper_black)
-# cv2.imshow("mask1", mask1)
-# detectShapes(image, mask1)
-
-# cv2.imshow('image', image)
-
+# cv2.imshow("thresh",thresh)
+# detectShapes(image, thresh)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
